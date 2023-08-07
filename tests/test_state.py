6e80f1588a65778d09cd0db1589f336188e5b7b3
Kyle Cain <kylecain0@gmail.com>
2023-08-07 13:28:43 -0400

    Added complete test suite for test_state.py and created a GitHub workflow file to run unittests on push.

--- tests/test_state.py
+++ tests/test_state.py
@@ -11,6 +11,14 @@ class GameStateTest(unittest.TestCase):
         # Assert that the position is None since no game state has been updated yet
         self.assertIsNone(position)
 
+    def test_update_game_state(self):
+        # Add test case for update_game_state method
+        pass
+
+    def test_get_game_time(self):
+        # Add test case for get_game_time method
+        pass
+
     # Add more test cases for other methods...
 
 if __name__ == "__main__":
